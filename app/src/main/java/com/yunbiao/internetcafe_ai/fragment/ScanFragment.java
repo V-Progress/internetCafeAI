473d0197233c2de334c4cf5cdb0001515bd96d75
JaneV <13032660329@163.com>
2020-08-11 07:59:32 +0800

    增加部分页面，增加读卡扫码工具类

--- app/src/main/java/com/yunbiao/internetcafe_ai/fragment/ScanFragment.java
+++ app/src/main/java/com/yunbiao/internetcafe_ai/fragment/ScanFragment.java
@@ -1,20 +1,35 @@
 package com.yunbiao.internetcafe_ai.fragment;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.Html;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.yunbiao.internetcafe_ai.DialogUtil;
 import com.yunbiao.internetcafe_ai.R;
 import com.yunbiao.internetcafe_ai.ScanKeyManager;
 
-public class ScanFragment extends BaseFragment{
+public class ScanFragment extends BaseFragment {
 
     private TextView tvTipsScan;
     private ImageView ivTipsScan;
     private final String success_tips = "您预约的座位在<font color='#F6EC1B'>A区089号</font>，请进行身份验证";
     private final String failed_tips = "您的预约码<font color='#F6EC1B'>已过期</font>";
+    private String mCode;
+
+    public static ScanFragment newInstance(Bundle bundle) {
+        ScanFragment scanFragment = new ScanFragment();
+        scanFragment.setArguments(bundle);
+        return scanFragment;
+    }
 
     @Override
     protected int getLayout() {
@@ -36,22 +51,7 @@ public class ScanFragment extends BaseFragment{
         test();
     }
 
-    private void test(){
-        fv(R.id.btn_test_success).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setSeat(success_tips,R.mipmap.seat_icon);
-            }
-        });
-        fv(R.id.btn_test_failed).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setSeat(failed_tips,R.mipmap.qrcode_exp);
-            }
-        });
-    }
-
-    private void setSeat(String tips,int imgId){
+    private void setSeat(String tips, int imgId) {
         ivTipsScan.setImageResource(imgId);
         tvTipsScan.setText(Html.fromHtml(tips));
         tvTipsScan.setVisibility(View.VISIBLE);
@@ -60,7 +60,48 @@ public class ScanFragment extends BaseFragment{
     @Override
     protected void onClickRight() {
         Log.e(TAG, "onClickRight: 点击了右边的按钮");
+
+        if (TextUtils.isEmpty(mCode)) {
+            DialogUtil.instance().show1BtnSampleTips(getActivity(), "请扫描二维码~", "请打开小程序，扫描您的二维码");
+            return;
+        }
+
         jumpFragment(new CompareFragment());
     }
 
+    /*===测试逻辑=================================================================*/
+    private void test() {
+        Log.e(TAG, "test: 1111111111111");
+
+        final EditText edtTest = fv(R.id.edt_test);
+        fv(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "onClick: 222222222222222" );
+
+                DialogUtil.instance().showProgress(getActivity(),"正在查询...");
+
+                String s = edtTest.getText().toString();
+                mCode = s;
+                if(TextUtils.isEmpty(s)){
+                    handler.sendEmptyMessageDelayed(0,2000);
+                } else {
+
+                    handler.sendEmptyMessageDelayed(1,2000);
+                }
+            }
+        });
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            DialogUtil.instance().dismissProgress();
+            if (msg.what == 0) {
+                setSeat(failed_tips, R.mipmap.qrcode_exp);
+            } else {
+                setSeat(success_tips, R.mipmap.seat_icon);
+            }
+        }
+    };
 }
